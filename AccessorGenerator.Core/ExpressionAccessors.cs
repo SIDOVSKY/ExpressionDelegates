b169706040917d35ea2991e708ed1a3d78e40322
Vadim Sedov <sidovsky.develop@gmail.com>
2020-10-22 16:33:53 +0700

    Extract type name building

--- AccessorGenerator.Core/ExpressionAccessors.cs
+++ AccessorGenerator.Core/ExpressionAccessors.cs
@@ -22,37 +22,9 @@ namespace AccessorGenerator.Core
 
         public static Accessor? Find(MemberInfo member)
         {
-            var fullType = BuildFullTypeName(member.DeclaringType).ToString();
-            var path = $"{fullType}.{member.Name}".Replace('+', '.');
+            var fullType = ReflectionNameBuilder.FullTypeName(member.DeclaringType).ToString();
+            var path = $"{fullType}.{member.Name}";
             return Find(path);
-
-            static StringBuilder BuildFullTypeName(Type type, StringBuilder? sb = null)
-            {
-                sb ??= new StringBuilder();
-
-                if (!type.IsGenericType)
-                {
-                    sb.Append(type.FullName);
-                    return sb;
-                }
-
-                sb.Append(type.FullName, 0, type.FullName.IndexOf('`'));
-                sb.Append('<');
-
-                var appendComma = false;
-                foreach (var typeParam in type.GetGenericArguments())
-                {
-                    if (appendComma)
-                    {
-                        sb.Append(", ");
-                    }
-
-                    BuildFullTypeName(typeParam, sb);
-                    appendComma = true;
-                }
-                sb.Append('>');
-                return sb;
-            };
         }
     }
 }
---
+++ AccessorGenerator.Core/ReflectionNameBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AccessorGenerator.Core
+{
+
+    public static class ReflectionNameBuilder
+    {
+        public static StringBuilder FullTypeName(Type type, StringBuilder? sb = null)
+        {
+            sb ??= new StringBuilder();
+
+            if (!type.IsGenericType)
+            {
+                sb.Append(type.FullName).Replace('+', '.');
+                return sb;
+            }
+
+            sb.Append(type.FullName, 0, type.FullName.IndexOf('`')).Replace('+', '.');
+            sb.Append('<');
+
+            var appendComma = false;
+            foreach (var typeParam in type.GetGenericArguments())
+            {
+                if (appendComma)
+                {
+                    sb.Append(", ");
+                }
+
+                FullTypeName(typeParam, sb);
+                appendComma = true;
+            }
+            sb.Append('>');
+            return sb;
+        }
+    }
+}
